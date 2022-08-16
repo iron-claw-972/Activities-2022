7768a5a66aeb2109f139e5b3a4c0eda003c54a7a
PandaNinja10 <78180593+PandaNinja10@users.noreply.github.com>
2022-08-15 21:08:56 -0700

    change TODO numbering

--- src/main/java/frc/robot/constants/DriveConstants.java
+++ src/main/java/frc/robot/constants/DriveConstants.java
@@ -3,7 +3,7 @@ package frc.robot.constants;
 public class DriveConstants {
 
   public final int kRightMotor1 = -1;
-  // TODO 1: Add the other motor ports, kLeftMotor1, kRightMotor2, kLeftMotor2, the same as above.
+  // TODO 1.1: Add the other motor ports, kLeftMotor1, kRightMotor2, kLeftMotor2, the same as above.
   // All of the ports should be public (so they can be accessed anywhere) and final (so they don't change)
   // find the ID through the motor label, electrical spreadsheet, or phoenix tuner (ask us for help)
   
--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -19,14 +19,14 @@ import frc.robot.util.MotorFactory;
 
 public class Drivetrain extends SubsystemBase {
 
-  // TODO 1: Create Motor Objects using MotorFactory.createTalonFX(int id)
+  // TODO 1.1: Create Motor Objects using MotorFactory.createTalonFX(int id)
   // the ID should be set in the DriveConstants.java file, here you can access it like Constants.drive.kRightMotor1
   WPI_TalonFX leftMotor1;
   WPI_TalonFX rightMotor1;
   WPI_TalonFX leftMotor2;
   WPI_TalonFX rightMotor2;
 
-  // TODO 2: Add two instances of PhoenixMotorControllerGroup(leadMotor, motor2), one for each side. 
+  // TODO 1.2: Add two instances of PhoenixMotorControllerGroup(leadMotor, motor2), one for each side. 
   PhoenixMotorControllerGroup leftMotors;
   PhoenixMotorControllerGroup rightMotors; 
 
@@ -34,7 +34,7 @@ public class Drivetrain extends SubsystemBase {
    * Creates a new DriveSubsystem.
    */
   public Drivetrain() {
-    // TODO 1: This constructor runs when the subsystem is created so you can do some setup here. Make the secondary motors follow the main ones.
+    // TODO 1.1: This constructor runs when the subsystem is created so you can do some setup here. Make the secondary motors follow the main ones.
     // You can also invert the motors, you often need to invert one side to make the robot drive since the motors on one side are flipped.
     // Examples for how are below, replace the variable motor with your motor variable and make sure you set the right motors! Do not only invert one motor on a side or it may mesh against the other motor
     // motor.setInverted(true);
@@ -42,7 +42,7 @@ public class Drivetrain extends SubsystemBase {
 
 
 
-    // TODO 2: Change all of the setup above. Motors in a group automatically follow each other so do not set them as followers. You can set them inverted as such:
+    // TODO 1.2: Change all of the setup above. Motors in a group automatically follow each other so do not set them as followers. You can set them inverted as such:
     // leftMotors.setInverted(true);
   }
 
@@ -53,7 +53,7 @@ public class Drivetrain extends SubsystemBase {
    * @param rightPower the commanded power to the right motors
    */
   public void tankDrive(double leftPower, double rightPower) {
-    // TODO 2: Instead of setting motors set the MotorControllerGroup, using motorGroup.set(speed)
+    // TODO 1.2: Instead of setting motors set the MotorControllerGroup, using motorGroup.set(speed)
     leftMotor1.set(ControlMode.PercentOutput, leftPower);
     rightMotor1.set(ControlMode.PercentOutput, rightPower);
   }
