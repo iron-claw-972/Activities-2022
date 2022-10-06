d6bfa43318fc21f4f5e346a3ab11243b0db52421
Kyle-Eldridge <113394349+Kyle-Eldridge@users.noreply.github.com>
2022-10-06 16:07:21 -0700

    123

--- src/main/java/frc/robot/controls/Driver.java
+++ src/main/java/frc/robot/controls/Driver.java
@@ -1,6 +1,8 @@
 package frc.robot.controls;
 
+import frc.robot.Robot;
 import frc.robot.commands.DoNothing;
+import frc.robot.commands.RunFor5Seconds;
 import frc.robot.constants.Constants;
 import lib.controllers.GameController;
 import lib.controllers.GameController.Axis;
@@ -12,7 +14,7 @@ public class Driver {
   public static void configureControls() {
 
     // TODO 3.1: Change the DoNothing() command to one of your commands
-    driver.get(Button.A).whenPressed(new DoNothing());
+    driver.get(Button.A).whenPressed(new RunFor5Seconds(Robot.drive));
 
     // TODO 3.3: Write some more triggers for your commands! Group your commands and functions using at least one of each of these: ParallelCommandGroup, SequentialCommandGroup, ConditionalCommand, PrintCommand, WaitUntilCommand
   }
