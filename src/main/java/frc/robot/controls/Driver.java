fda808468918c775cb52aab8432f8fbfa1aad598
MagicEmu <yichendai3@gmail.com>
2022-10-19 15:03:53 -0700

    did 3.3

--- src/main/java/frc/robot/controls/Driver.java
+++ src/main/java/frc/robot/controls/Driver.java
@@ -1,11 +1,13 @@
 package frc.robot.controls;
 
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Robot;
 import frc.robot.commands.DoNothing;
 import frc.robot.commands.RunUntilCondition;
 import frc.robot.constants.Constants;
 import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.SideOfDrivetrain;
 import lib.controllers.GameController;
 import lib.controllers.GameController.Axis;
 import lib.controllers.GameController.Button;
@@ -19,7 +21,7 @@ public class Driver {
     driver.get(Button.A).whenPressed(new RunUntilCondition(Robot.drive));
 
     // TODO 3.3: Write some more triggers for your commands! Group your commands and functions using at least one of each of these: ParallelCommandGroup, SequentialCommandGroup, ConditionalCommand, PrintCommand, WaitUntilCommand
-    
+    driver.get(Button.X).whenPressed(new SequentialCommandGroup(new RunCommand(()-> new SideOfDrivetrain(true).dance(), Robot.subDrive), new RunUntilCondition(Robot.drive)));
   }
 
   public static double getRawThrottleValue() {
